721fbf3bb99c31b5298fd0f88b6847d6f79df181
SHERRY <swasieko@gmail.com>
2017-04-14 20:03:29 +0000

    Implemented user connection to posts with tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Post, type: :model do
   describe "Creation" do
     before do 
       @post = Post.create(date: Date.today, rationale: "anything")
+      @user = User.create(first_name: "Jonas", last_name: "Sniegas")
+      @post.user = @user
     end
     
     it "can be created" do
