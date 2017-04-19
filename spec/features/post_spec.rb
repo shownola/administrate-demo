ab205d5723834f68f8d5f5da8ac206049982af0c
SHERRY <swasieko@gmail.com>
2017-04-19 20:11:26 +0000

    Implemented new index query for showing users posts they created and hiding others

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,6 +26,20 @@ require 'rails_helper'
         visit posts_path
         expect(page).to have_content(/Rationale|content/)
       end
+      
+      it 'has a scope so that only post creators can see their post' do
+        post1 = Post.create(date: Date.today, rationale: "other rationale", user_id: @user.id)
+        post2 =  Post.create(date: Date.today, rationale: "more rationale", user_id: @user.id)
+        
+        other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@email.com", password: "password", password_confirmation: "password")
+    
+        post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+        
+        visit posts_path
+        
+        expect(page).to_not have_content(/This post shouldn't be seen/)
+        
+      end
     end
     
     describe 'new' do
@@ -40,6 +54,7 @@ require 'rails_helper'
     describe 'delete' do
       it 'can be deleted' do
         @post = FactoryGirl.create(:post)
+        @post.update(user_id: @user.id)
         visit posts_path
         
         click_link ("delete_post_#{@post.id}_from_index")
