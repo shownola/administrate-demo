e9813e680ef167a377f0f1cb809e1d0affe832da
SHERRY <swasieko@gmail.com>
2017-04-19 17:17:38 +0000

    Implemented tests and page redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -19,5 +19,13 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
   end
+  
+  factory :non_authorized_user, class: "User" do
+    first_name 'Non'
+    last_name 'Authorized'
+    email { generate :email}
+    password "password"
+    password_confirmation "password"
+  end
 end
 
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -76,17 +76,14 @@ require 'rails_helper'
     
     describe 'edit' do
       before do
-        @post = FactoryGirl.create(:post)
-      end
-      it 'can be reached by clicking edit on the index page' do
-        visit posts_path
-        
-        click_link ("edit_#{@post.id}")
-        expect(page.status_code).to eq(200)
+       @edit_user = User.create(first_name: "Snowam", last_name: "Shephard", email: "sam@email", password: "password", password_confirmation: "password")
+       login_as(@edit_user, :scope => :user)
+       @edit_post = Post.create(date: Date.today, rationale: "something went wrong", user_id: @edit_user.id)
       end
       
+      
       it 'can be edited' do
-        visit edit_post_path(@post)
+        visit edit_post_path(@edit_post)
         
         fill_in 'post[date]', with: Date.today
         fill_in 'post[rationale]', with: "Edited content"
@@ -94,6 +91,16 @@ require 'rails_helper'
 
         expect(page).to have_content("Edited content")
         
+      end
+      
+      it 'cannot be edited by a non authorized user' do
+        logout(:user)
+        non_authorized_user = FactoryGirl.create(:non_authorized_user)
+        login_as(non_authorized_user, :scope => :user)
+        
+        visit edit_post_path(@edit_post)
+        
+        expect(current_path).to eq(root_path)
       end
     end
 end
