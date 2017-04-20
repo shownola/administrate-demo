ab60c45222c216675b25de8f17791237c070085a
SHERRY <swasieko@gmail.com>
2017-04-20 00:30:17 +0000

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ AdminUser.create!(email: "admin@email.com", password: "password", password_confi
 puts "1 admin user created"
 
 50.times do |post| 
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 puts "50 posts have been created"
