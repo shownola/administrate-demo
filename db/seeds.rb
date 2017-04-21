d41fb2a57e1695c79cf1ecfbe06bc85942a99e11
SHERRY <swasieko@gmail.com>
2017-04-21 22:08:04 +0000

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ AdminUser.create!(email: "admin@email.com",
 puts "1 admin user created"
 
 50.times do |post| 
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content In ut lectus vel elit egestas accumsan in nec leo. Maecenas auctor turpis ut velit dictum molestie. Mauris in blandit neque. Fusce non nisi quis sapien pulvinar posuere in ut dolor. Nullam ultrices, diam a feugiat congue, quam nibh dictum tellus, eget cursus tortor enim eget nulla. Donec placerat est augue, ut accumsan diam imperdiet vel. Phasellus ac interdum elit, vel malesuada velit. Suspendisse rhoncus lacus erat, pretium viverra magna interdum at. Aliquam auctor lacus ut sem maximus, vitae ullamcorper ante finibus. Vivamus eget urna eu risus mattis imperdiet sit amet quis urna.", user_id: @user.id, overtime_request: 2.5)
 end
 puts "50 posts have been created"
 
