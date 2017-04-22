e923a36cfd763961d695946a965c776889315e0a
SHERRY <swasieko@gmail.com>
2017-04-22 07:14:51 +0000

    Setup employee home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+   def admin_types
+    ['AdminUser']
+   end
   
   private
   
