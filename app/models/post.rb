097ea1d760d9b2cd1cde9f56ec1098897f930719
SHERRY <swasieko@gmail.com>
2017-04-22 23:59:21 +0000

    Fixed a typo in the post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
   private
   
   def update_audit_log
-    audit_log = AuditLog.where(user_id: slef.user_id, start_date: (self.date - 7.days..self.date)).last
+    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
     audit_log.confirmed!
   end
 end
