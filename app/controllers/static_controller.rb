07f75c1d40eda6270645eeca43287e611c9d89b8
SHERRY <swasieko@gmail.com>
2017-04-22 18:15:44 +0000

    Styled the employee homepage pending audit approvals

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,10 +2,10 @@ class StaticController < ApplicationController
 
     def homepage
         if admin_types.include?(current_user.type) 
-            @pending_approvals = Post.submitted
-            @recent_audit_items = AuditLog.last(10)
+          @pending_approvals = Post.submitted
+          @recent_audit_items = AuditLog.last(10)
         else
-            
+          @pending_audit_confirmations = current_user.audit_logs  
         end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,15 @@ AdminUser.create!(email: "admin@email.com",
                   phone: "5555555555") 
 puts "1 admin user created"
 
-50.times do |post| 
+30.times do |post| 
   Post.create!(date: Date.today, rationale: "#{post} rationale content In ut lectus vel elit egestas accumsan in nec leo. Maecenas auctor turpis ut velit dictum molestie. Mauris in blandit neque. Fusce non nisi quis sapien pulvinar posuere in ut dolor. Nullam ultrices, diam a feugiat congue, quam nibh dictum tellus, eget cursus tortor enim eget nulla. Donec placerat est augue, ut accumsan diam imperdiet vel. Phasellus ac interdum elit, vel malesuada velit. Suspendisse rhoncus lacus erat, pretium viverra magna interdum at. Aliquam auctor lacus ut sem maximus, vitae ullamcorper ante finibus. Vivamus eget urna eu risus mattis imperdiet sit amet quis urna.", user_id: @user.id, overtime_request: 2.5)
 end
-puts "50 posts have been created"
+puts "30 posts have been created"
 
-50.times do |audit_log| 
-  AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days)) 
-end
-puts "50 audit logs have been created"
+ 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days)) 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
 
