22ae1e6c573f6e7e26e054fa7ab9198b5d63a847
SHERRY <swasieko@gmail.com>
2017-04-22 20:41:24 +0000

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+
 end
