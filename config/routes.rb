ecf6b2da2440638dc4206e5019ec9e774e27ab60
SHERRY <swasieko@gmail.com>
2017-04-24 19:40:31 +0000

    Fixed bug in admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :users
-      resources :posts
+    resources :posts
     resources :admin_users
+    resources :employees
       
 
     root to: "users#index"
